1830a176d980f0ebb8d1ca62f7596be0a644d87c
julianojnc <jncjuliano@gmail.com>
2025-05-27 19:13:30 -0300

    Novo layout da tela de Perfil

--- lib/core/styles/styles.dart
+++ lib/core/styles/styles.dart
@@ -7,4 +7,6 @@ class Styles {
   static final Color lineBorderColor = Color(0xffa4a4a4);
   static final Color linkColor = Color(0xffC9A77B);
   static final Color textFieldColor = Color(0xffd5c59f);
+  static final Color button = Color(0xfff6ba7e);
+  static final Color buttonSecond = Color(0xffd6c69f);
 }
--- lib/presentetion/pages/profile_screen.dart
+++ lib/presentetion/pages/profile_screen.dart
@@ -1,5 +1,5 @@
+import 'package:app_praca_ciencia/core/styles/styles.dart';
 import 'package:app_praca_ciencia/core/widgets/header.dart';
-import 'package:app_praca_ciencia/core/widgets/menu.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -10,113 +10,357 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool showProfile = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(title: 'Perfil'),
-      drawer: Menu(),
       body: Container(
-        color: const Color(0xFFFFFFFF),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Profile Header
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 60),
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment(0.8, 1),
-                    colors: <Color>[Color(0xFFFFEA00), Color(0xFFFF6A00)],
-                    tileMode: TileMode.mirror,
-                  ),
-                ),
-                child: const Icon(
-                  Icons.account_circle,
-                  size: 130,
-                  color: Color(0xFFFFFFFF),
-                ),
+        width: double.infinity,
+        height: double.infinity,
+        color: Styles.backgroundColor,
+        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 20),
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(14),
               ),
-              // Form Section
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 25,
-                ),
+              child: Icon(
+                Icons.account_circle,
+                size: 130,
+                color: Color(0xFFFFFFFF),
+              ),
+            ),
+            // Navegacao
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildTabButton('Dados Pessoais', showProfile, () {
+                  setState(() {
+                    showProfile = true;
+                  });
+                }),
+                SizedBox(width: 10),
+                _buildTabButton('Agendamentos', !showProfile, () {
+                  setState(() {
+                    showProfile = false;
+                  });
+                }),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child:
+                    showProfile
+                        ? _buildProfileSection()
+                        : _buildAgendamentoSection(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Abas Style
+  Widget _buildTabButton(String label, bool isSelected, VoidCallback onTap) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isSelected ? Styles.button : Colors.grey[400],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          color: isSelected ? Styles.fontColor : Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+
+  // Dados do Perfil
+  Widget _buildProfileSection() {
+    return Column(
+      children: [
+        _buildTextField('Nome do Usuário', 'cadProfileIcon'),
+        const SizedBox(height: 16),
+        _buildTextField('Data de Nascimento', 'cadDateIcon'),
+        const SizedBox(height: 16),
+        _buildTextField('CPF', 'cadDocIcon'),
+        const SizedBox(height: 16),
+        _buildTextField('Email', 'cadEmailIcon'),
+        const SizedBox(height: 16),
+        _buildTextField('Telefone', 'cadPhoneIcon'),
+        const SizedBox(height: 16),
+        _buildTextField('Endereço', 'cadLocalIcon'),
+        const SizedBox(height: 24),
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Styles.button),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: BorderSide(color: Styles.fontColor),
+              ),
+            ),
+          ),
+          onPressed: () {},
+          child: Text(
+            'EDITAR',
+            style: TextStyle(
+              color: Styles.fontColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Agendamentos
+  Widget _buildAgendamentoSection() {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            'Oficinas',
+            style: TextStyle(
+              color: Styles.fontColor,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.start,
+          ),
+        ),
+
+        Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            color: Styles.backgroundContentColor,
+            borderRadius: BorderRadius.circular(14),
+          ),
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage('assets/images/imgOficina.png'),
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
                 child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Align text to left
                   children: [
-                    _buildLabel('Nome Completo'),
-                    _buildTextField('Nome do Usuário'),
+                    Text(
+                      'Relógio de sol',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '10 - 10',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'MAI - MAI',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
 
-                    const SizedBox(height: 16),
-                    _buildLabel('CPF'),
-                    _buildTextField('CPF do Usuário'),
-
-                    const SizedBox(height: 16),
-                    _buildLabel('Endereço'),
-                    _buildTextField('Endereço do Usuário'),
-
-                    const SizedBox(height: 16),
-                    _buildLabel('Bairro'),
-                    _buildTextField('Bairro do Usuário'),
-
-                    const SizedBox(height: 16),
-                    _buildLabel('Data de nascimento'),
-                    _buildTextField('Data de nascimento do Usuário'),
-
-                    const SizedBox(height: 16),
-                    _buildLabel('Telefone'),
-                    _buildTextField('Telefone do Usuário'),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Styles.buttonSecond,
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                side: BorderSide(color: Styles.fontColor),
+                              ),
+                            ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Informações',
+                        style: TextStyle(
+                          color: Styles.fontColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
             ],
           ),
         ),
-      ),
+
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            'Visitas',
+            style: TextStyle(
+              color: Styles.fontColor,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.start,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            color: Styles.backgroundContentColor,
+            borderRadius: BorderRadius.circular(14),
+          ),
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage('assets/images/imgOficina.png'),
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    Text(
+                      'Relógio de sol',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '10 - 10',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'MAI - MAI',
+                      style: TextStyle(
+                        color: Styles.fontColor,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Styles.buttonSecond,
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                                side: BorderSide(color: Styles.fontColor),
+                              ),
+                            ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Informações',
+                        style: TextStyle(
+                          color: Styles.fontColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Styles.button),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: BorderSide(color: Styles.fontColor),
+              ),
+            ),
+          ),
+          onPressed: () {},
+          child: Text(
+            'EDITAR',
+            style: TextStyle(
+              color: Styles.fontColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
+}
 
-  Widget _buildLabel(String text) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8, left: 4),
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          color: Color(0xFF424242),
+Widget _buildTextField(String hintText, String pathImg) {
+  return SizedBox(
+    child: PhysicalModel(
+      borderRadius: BorderRadius.circular(50),
+      color: Styles.textFieldColor,
+      elevation: 2,
+      shadowColor: Colors.black26,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+          color: Styles.textFieldColor,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Row(
+          children: [
+            Image(
+              image: AssetImage('assets/images/' + pathImg + '.png'),
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(width: 12),
+            Container(width: 1, height: 30, color: Colors.grey.shade600),
+            const SizedBox(width: 12),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: hintText,
+                  hintStyle: TextStyle(color: Styles.fontColor, fontSize: 15),
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Widget _buildTextField(String hintText) {
-    return PhysicalModel(
-      borderRadius: BorderRadius.circular(25),
-      color: Color(0xFFFFFFFF),
-      elevation: 5.0,
-      shadowColor: Color(0xFFF5F5F5),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: hintText,
-          filled: true,
-          fillColor: const Color(0xFFF5F5F5),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(24),
-            borderSide: BorderSide.none,
-          ),
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 14,
-          ),
-          hintStyle: TextStyle(color: Colors.grey[600]),
-        ),
-        style: const TextStyle(color: Color(0x00000000)),
-      ),
-    );
-  }
+    ),
+  );
 }
